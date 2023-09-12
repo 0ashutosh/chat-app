4d4a0ec1e4675b38be87bf28adfe5fc4c02b3635
perpendicular01 <53850029+0ashutosh@users.noreply.github.com>
2023-09-12 02:17:40 -0700

    Add files via upload

---
+++ chat-room/src/main/java/com/project1/webapp/chatroom/controller/MessageType.java
@@ -0,0 +1,8 @@
+package com.project1.webapp.chatroom.controller;
+
+public enum MessageType {
+
+	CHAT,
+	JOIN,
+	LEAVE,
+}
---
+++ chat-room/src/test/java/com/project1/webapp/chatroom/ChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.webapp.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
